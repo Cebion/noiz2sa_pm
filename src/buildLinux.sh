b4469c4f89fa8ea21ffcb33877917c21e488a8b2
root <7762208+Cebion@users.noreply.github.com>
2025-03-25 20:29:06 +0000

    Remove x64 specifics

--- src/buildLinux.sh
+++ src/buildLinux.sh
@@ -2,4 +2,4 @@
 rm *.o*
 gcc -c `sdl2-config --cflags` -O3 -Wall *.c
 g++ -c `sdl2-config --cflags` -O3 -Wall *.cc -I./bulletml/
-gcc -o noiz2sa *.o* `sdl2-config --libs` -lSDL2_mixer -lbulletml -lstdc++ -lm -L./lib/x64
+gcc -o noiz2sa *.o* `sdl2-config --libs` -lSDL2_mixer -lbulletml -lstdc++ -lm
